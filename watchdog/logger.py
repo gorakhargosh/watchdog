9485686cffabc3a1e9efacc27f6c7fda20dfe3d7
Gora Khargosh <gora.khargosh@gmail.com>
2010-11-15 18:35:47 +0530

    Update temporary logger
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- watchdog/logger.py
+++ watchdog/logger.py
@@ -3,9 +3,8 @@
 import logging
 
 logging.basicConfig(level=logging.DEBUG,
-                    format='%(pathname)s/%(funcName)s/(%(threadName)-10s) %(message)s',
+                    format='(%(threadName)-10s) %(message)s',
                     )
 
 def debug(format_string, *args, **kwargs):
     logging.debug(format_string, *args, **kwargs)
-    
