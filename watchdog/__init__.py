3d1cfa3b0e24d9fd282956f143c348f01f67bd0e
Gora Khargosh <gora.khargosh@gmail.com>
2010-11-15 23:21:15 +0530

    Import as instead of assignment in watchdog/__init__.py
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- watchdog/__init__.py
+++ watchdog/__init__.py
@@ -4,9 +4,7 @@ from events import FileSystemEventHandler
 
 try:
     import _fsevents
-    from fsevents_observer import FSEventsObserver
-    Observer = FSEventsObserver
+    from fsevents_observer import FSEventsObserver as Observer
 except ImportError:
-    from polling_observer import PollingObserver
-    Observer = PollingObserver
+    from polling_observer import PollingObserver as Observer
 
