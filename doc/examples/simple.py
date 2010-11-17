7d298678f7acb6c4736a0693f798521f70599947
Gora Khargosh <gora.khargosh@gmail.com>
2010-11-17 16:19:42 +0530

    Add an example file named simple.py to documentation.
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

---
+++ doc/examples/simple.py
@@ -0,0 +1,35 @@
+import sys
+import time
+from watchdog import Observer, FileSystemEventHandler
+import logging 
+
+logging.basicConfig(level=logging.DEBUG)
+
+class MyEventHandler(FileSystemEventHandler):
+    def catch_all_handler(self, event):
+        logging.debug(event)
+
+    def on_moved(self, event):
+        self.catch_all_handler(event)
+
+    def on_created(self, event):
+        self.catch_all_handler(event)
+
+    def on_deleted(self, event):
+        self.catch_all_handler(event)
+
+    def on_modified(self, event):
+        self.catch_all_handler(event)
+
+
+event_handler = MyEventHandler()
+observer = Observer()
+observer.schedule('a-unique-name', event_handler, *sys.argv[1:])
+observer.start()
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.unschedule('a-unique-name')
+    observer.stop()
+observer.join()
