f9a22a0db46a84508cd7a6e5294612eb0484805a
Gora Khargosh <gora.khargosh@gmail.com>
2010-11-17 19:23:34 +0530

    Use InotifyObserver if the pyinotify dependency is satisfied.
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- watchdog/__init__.py
+++ watchdog/__init__.py
@@ -7,16 +7,20 @@ logging.basicConfig(level=logging.DEBUG)
 from events import FileSystemEventHandler
 
 try:
-    import _watchdog_fsevents
-    logging.debug('Using FSEventsObserver.')
-    from fsevents_observer import FSEventsObserver as Observer
+    import pyinotify
+    logging.debug('Using InotifyObserver')
+    from inotify_observer import InotifyObserver as Observer
 except ImportError:
     try:
-        import win32file
-        import win32con
-        logging.debug('Using Win32Observer.')
-        from win32_observer import Win32Observer as Observer
+        import _watchdog_fsevents
+        logging.debug('Using FSEventsObserver.')
+        from fsevents_observer import FSEventsObserver as Observer
     except ImportError:
-        logging.debug('Using PollingObserver as fallback.')
-        from polling_observer import PollingObserver as Observer
-
+        try:
+            import win32file
+            import win32con
+            logging.debug('Using Win32Observer.')
+            from win32_observer import Win32Observer as Observer
+        except ImportError:
+            logging.debug('Using PollingObserver as fallback.')
+            from polling_observer import PollingObserver as Observer
