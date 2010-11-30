a24716d3b42de0e9397b0dcc1a13b97d18e210c4
Gora Khargosh <gora.khargosh@gmail.com>
2010-11-30 13:00:47 +0530

    Ignore directory events in autobuild.sh
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- autobuild.sh
+++ autobuild.sh
@@ -5,11 +5,14 @@
 # Public domain.
 #
 
+# First forced build.
 make -C docs html
 
+# Monitored builds.
 bin/python watchmedo shell-command \
     --patterns="*.rst;*.rst.inc;*.py;*.py.swp;*.rst.swp;*.rst.inc.swp" \
     --ignore-patterns=".*;*~" \
+    --ignore-directories \
     --recursive \
     --command='make -C docs html' \
     watchdog/ docs/source/
