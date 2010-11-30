7638846847bb5e7a730fb114e897c802bdeec6a0
Gora Khargosh <gora.khargosh@gmail.com>
2010-11-30 23:40:30 +0530

    Use explicit locking and clean up API in PollingObserver.
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- watchdog/observers/polling_observer.py
+++ watchdog/observers/polling_observer.py
@@ -34,7 +34,6 @@ except ImportError:
 from watchdog.utils.collections import OrderedSetQueue
 from watchdog.utils import real_absolute_path, absolute_path, DaemonThread
 from watchdog.utils.dirsnapshot import DirectorySnapshot
-from watchdog.decorator_utils import synchronized
 from watchdog.events import \
     DirMovedEvent, \
     DirDeletedEvent, \
@@ -132,89 +131,94 @@ class PollingObserver(DaemonThread):
     """
     def __init__(self, interval=1):
         super(PollingObserver, self).__init__(interval)
-        self.event_queue = OrderedSetQueue()
-        self.event_emitters = set()
-        self.rules = {}
-        self.map_name_to_paths = {}
+        
+        self._lock = threading.RLock()
+        
+        self._event_queue = OrderedSetQueue()
+        self._event_emitters = set()
+        self._rules = {}
+        self._map_name_to_paths = {}
 
+    @property
+    def event_queue(self):
+        return self._event_queue
+        
 
     # The win32/kqueue observers override this method because all of the other
     # functionality of this class is the same as its own.
     def _create_event_emitter(self, path, recursive):
         return _PollingEventEmitter(path=path,
                                     interval=self.interval,
-                                    out_event_queue=self.event_queue,
+                                    out_event_queue=self._event_queue,
                                     recursive=recursive)
 
 
-    @synchronized()
     def schedule(self, name, event_handler, paths=None, recursive=False):
         """Schedules monitoring specified paths and calls methods in the
         given callback handler based on events occurring in the file system.
         """
-        if not paths:
-            raise ValueError('Please specify a few paths.')
-        if isinstance(paths, basestring):
-            paths = [paths]
+        with self._lock:
+            if not paths:
+                raise ValueError('Please specify a few paths.')
+            if isinstance(paths, basestring):
+                paths = [paths]
+    
+            self._map_name_to_paths[name] = set()
+            for path in paths:
+                if not isinstance(path, basestring):
+                    raise TypeError("Path must be string, not '%s'." % type(path).__name__)
+                path = real_absolute_path(path)
+                self._schedule_path(name, event_handler, recursive, path)
 
-        self.map_name_to_paths[name] = set()
-        for path in paths:
-            if not isinstance(path, basestring):
-                raise TypeError("Path must be string, not '%s'." % type(path).__name__)
-            path = real_absolute_path(path)
-            self._schedule_path(name, event_handler, recursive, path)
 
-
-    @synchronized()
     def _schedule_path(self, name, event_handler, recursive, path):
         """Starts monitoring the given path for file system events."""
-        if os.path.isdir(path) and not path in self.rules:
-            event_emitter = self._create_event_emitter(path=path, recursive=recursive)
-            self.event_emitters.add(event_emitter)
-            self.rules[path] = _Rule(path=path,
-                                    event_handler=event_handler,
-                                    event_emitter=event_emitter)
-            self.map_name_to_paths[name].add(path)
-            event_emitter.start()
+        with self._lock:
+            if os.path.isdir(path) and not path in self._rules:
+                event_emitter = self._create_event_emitter(path=path, recursive=recursive)
+                self._event_emitters.add(event_emitter)
+                self._rules[path] = _Rule(path=path,
+                                        event_handler=event_handler,
+                                        event_emitter=event_emitter)
+                self._map_name_to_paths[name].add(path)
+                event_emitter.start()
 
 
-    @synchronized()
     def unschedule(self, *names):
         """Stops monitoring specified paths for file system events."""
-        if not names:
-            for name, path in self.map_name_to_paths.items():
-                self._unschedule_path(path)
-        else:
-            for name in names:
-                for path in self.map_name_to_paths[name]:
+        with self._lock:
+            if not names:
+                for name, path in self._map_name_to_paths.items():
                     self._unschedule_path(path)
-                del self.map_name_to_paths[name]
+            else:
+                for name in names:
+                    for path in self._map_name_to_paths[name]:
+                        self._unschedule_path(path)
+                    del self._map_name_to_paths[name]
 
 
-    @synchronized()
     def _unschedule_path(self, path):
         """Stops watching a given path if already being monitored."""
-        if path in self.rules:
-            rule = self.rules.pop(path)
-            rule.event_emitter.stop()
-            self.event_emitters.remove(rule.event_emitter)
+        with self._lock:
+            if path in self._rules:
+                rule = self._rules.pop(path)
+                rule.event_emitter.stop()
+                self._event_emitters.remove(rule.event_emitter)
 
 
     def run(self):
-        """Dispatches events from the event queue to the callback handler."""
         while not self.is_stopped:
-            #logging.debug('runloop')
             try:
-                (rule_path, event) = self.event_queue.get(block=True, timeout=self.interval)
-                rule = self.rules[rule_path]
+                (rule_path, event) = self._event_queue.get(block=True, timeout=self.interval)
+                rule = self._rules[rule_path]
                 rule.event_handler.dispatch(event)
-                self.event_queue.task_done()
+                self._event_queue.task_done()
             except queue.Empty:
-                #logging.debug('queue empty')
                 continue
 
+
     def on_stopping(self):
-        for event_emitter in self.event_emitters:
+        for event_emitter in self._event_emitters:
             event_emitter.stop()
 
 
