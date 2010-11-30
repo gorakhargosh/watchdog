3cd640718f29dfc46d86903f55976a7bcc1d251e
Gora Khargosh <gora.khargosh@gmail.com>
2010-11-30 17:45:10 +0530

    Update manifest and remove setuptools usage
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- setup.py
+++ setup.py
@@ -25,9 +25,7 @@ import sys
 import imp
 
 from os.path import join as path_join, dirname
-from setuptools import setup, find_packages
-from setuptools.extension import Extension
-from setuptools.command.build_ext import build_ext
+from distutils.core import setup, Extension
 from distutils.util import get_platform
 
 version = imp.load_source('version', path_join('watchdog', 'version.py'))
@@ -80,18 +78,18 @@ ext_modules = {
     PLATFORM_WINDOWS: [],
 }
 
-common_install_requires = ['PyYAML >= 3.09', 'argh >= 0.8.1']
+common_install_requires = ['PyYAML (>=3.09)', 'argh (>=0.8.1)']
 if sys.version_info < (2, 7, 0) and \
         ('linux' in sys.platform or 'bsd' in sys.platform or 'darwin' in sys.platform):
     # Python 2.6 and below have the broken/non-existent kqueue implementations in the
     # select module. This backported patch adds it.
-    common_install_requires.append('select_backport >= 0.2')
-    common_install_requires.append('argparse >= 1.1')
+    common_install_requires.append('select_backport (>=0.2)')
+    common_install_requires.append('argparse (>=1.1)')
 
 install_requires = {
     PLATFORM_MACOSX: [],
-    PLATFORM_LINUX: ['pyinotify >= 0.9.1'],
-    PLATFORM_WINDOWS: ['pywin32 >= 214'],
+    PLATFORM_LINUX: ['pyinotify (>=0.9.1)'],
+    PLATFORM_WINDOWS: ['pywin32 (>=214)'],
 }
 
 scripts = [
@@ -109,14 +107,12 @@ setup(
     author="Gora Khargosh",
     author_email="gora.khargosh@gmail.com",
     license="MIT License",
-    cmdclass=dict(build_ext=build_ext),
     url="http://github.com/gorakhargosh/watchdog",
     download_url="http://watchdog-python.googlecode.com/files/watchdog-%s.tar.gz" % version.VERSION_STRING,
     keywords = "python filesystem monitoring monitor fsevents inotify",
     classifiers=trove_classifiers,
     ext_modules=ext_modules.get(platform, []),
-    packages=find_packages(),
+    packages=['watchdog'],
     scripts=scripts,
-    zip_safe=False,
-    install_requires=common_install_requires + install_requires.get(platform, []),
+    requires=common_install_requires + install_requires.get(platform, []),
     )
