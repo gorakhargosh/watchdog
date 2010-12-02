27ee09f26fa52b73e6fc2e421c02df29c8038851
Gora Khargosh <gora.khargosh@gmail.com>
2010-12-02 19:38:46 +0530

    Add coverage support to nosetests
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

---
+++ tests/run_tests.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from os.path import dirname, abspath, normpath, join as path_join
+
+import nose
+
+def absolute_path(path):
+    return abspath(normpath(path))
+
+dir_path = absolute_path(dirname(__file__))
+parent_dir_path = dirname(dir_path)
+sys.path[0:0] = [parent_dir_path]
+
+from watchdog.utils import platform
+
+# Explicitly define which packages/modules to cover.
+cover_packages = [
+    'watchdog.version',
+    'watchdog.events',
+#    'watchdog.utils.echo',
+    'watchdog.utils.dirsnapshot',
+    'watchdog.utils.collections.ordered_set_queue',
+    'watchdog.observers.polling_observer',
+]
+cover_packages_windows = [
+    'watchdog.observers.win32_observer',
+    'watchdog.observers.win32ioc_observer',
+]
+cover_packages_bsd = [
+    'watchdog.observers.kqueue_observer',
+]
+cover_packages_darwin = [
+    'watchdog.observers.fsevents_observer',
+    'watchdog.observers.kqueue_observer',
+]
+cover_packages_linux = [
+    'watchdog.observers.inotify_observer',
+]
+
+if platform.is_windows():
+    cover_packages.extend(cover_packages_windows)
+elif platform.is_darwin():
+    cover_packages.extend(cover_packages_darwin)
+elif platform.is_bsd():
+    cover_packages.extend(cover_packages_bsd)
+elif platform.is_linux():
+    cover_packages.extend(cover_packages_linux)
+
+
+if __name__ == "__main__":
+    config_path = path_join(parent_dir_path, 'nose.cfg')
+
+    argv = [__file__]
+    argv += '--with-doctest --with-coverage'.split()
+    argv += ['--cover-package=%s' % ','.join(cover_packages)]
+    #argv += ['--config=%s' % config_path]
+    argv += ['--verbosity=3']
+    nose.run(argv=argv)
