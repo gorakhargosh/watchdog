d7b4722cd03545b62081ba63bb25aa1915a8987e
Gora Khargosh <gora.khargosh@gmail.com>
2010-12-04 22:51:01 +0530

    Remove unnecessary code from PatternMatchingEventHandler
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- watchdog/events.py
+++ watchdog/events.py
@@ -367,53 +367,6 @@ class PatternMatchingEventHandler(FileSystemEventHandler):
             event_type = event.event_type
             _method_map[event_type](event)
 
-    def on_any_event(self, event):
-        """Catch-all event handler.
-
-        :param event:
-            The event object representing the file system event.
-        :type event:
-            :class:`FileSystemEvent`
-        """
-
-
-    def on_moved(self, event):
-        """Called when a file or a directory is moved or renamed.
-
-        :param event:
-            Event representing file/directory movement.
-        :type event:
-            :class:`DirMovedEvent` or :class:`FileMovedEvent`
-        """
-
-    def on_created(self, event):
-        """Called when a file or directory is created.
-
-        :param event:
-            Event representing file/directory creation.
-        :type event:
-            :class:`DirCreatedEvent` or :class:`FileCreatedEvent`
-        """
-
-
-    def on_deleted(self, event):
-        """Called when a file or directory is deleted.
-
-        :param event:
-            Event representing file/directory deletion.
-        :type event:
-            :class:`DirDeletedEvent` or :class:`FileDeletedEvent`
-        """
-
-    def on_modified(self, event):
-        """Called when a file or directory is modified.
-
-        :param event:
-            Event representing file/directory modification.
-        :type event:
-            :class:`DirModifiedEvent` or :class:`FileModifiedEvent`
-        """
-
 
 class LoggingEventHandler(FileSystemEventHandler):
     """Logs all the events captured."""
