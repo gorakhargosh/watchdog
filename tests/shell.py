5cce428fe8ed2e6313e1963188f4b8eedb68430d
Gora Khargosh <gora.khargosh@gmail.com>
2010-12-06 20:51:27 +0530

    Move shell test utils to tests/shell.py module.
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

---
+++ tests/shell.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+"""
+    :module: tests.shell
+    :synopsis: Common shell operations for testing.
+    :author: Gora Khargosh <gora.khargosh@gmail.com>
+"""
+
+import sys
+import os
+import time
+import os.path
+import tempfile
+import shutil
+import errno
+
+
+#def tree(path='.', show_files=False):
+#    print(path)
+#    padding = ''
+#    for root, directories, filenames in os.walk(path):
+#        print(padding + os.path.basename(root) + os.path.sep)
+#        padding = padding + '   '
+#        for filename in filenames:
+#            print(padding + filename)
+
+
+def cd(path):
+    os.chdir(path)
+
+
+def pwd():
+    path = os.getcwd()
+    print(path)
+    return path
+
+
+def mkdir(path, parents=False):
+    """Creates a directory (optionally also creates all the parent directories
+    in the path)."""
+    if parents:
+        try:
+            os.makedirs(path)
+        except OSError, e:
+            if not e.errno == errno.EEXIST:
+                raise
+    else:
+        os.mkdir(path)
+
+
+def rm(path, recursive=False):
+    """Deletes files or directories."""
+    if os.path.isdir(path):
+        if recursive:
+            shutil.rmtree(path)
+        #else:
+        #    os.rmdir(path)
+        else:
+            raise OSError("rm: %s: is a directory." % path)
+    else:
+        os.remove(path)
+
+
+def touch(path, times=None):
+    """Updates the modified timestamp of a file or directory."""
+    if os.path.isdir(path):
+        os.utime(path, times)
+    else:
+        with open(path, 'ab'):
+            os.utime(path, times)
+
+
+def truncate(path):
+    """Truncates a file."""
+    with open(path, 'wb'):
+        os.utime(path, None)
+
+
+def mv(src_path, dest_path):
+    """Moves files or directories."""
+    shutil.move(src_path, dest_path)
+
+
+def mkdtemp():
+    return tempfile.mkdtemp()
--- tests/utils.py
+++ tests/utils.py
@@ -3,14 +3,8 @@
 
 from __future__ import with_statement
 
-import sys
-import os
-import time
-import os.path
-import tempfile
-import shutil
-
 from nose.tools import assert_raises
+
 
 def assert_readonly_public_attributes(o):
     for prop in list_attributes(o, True):
@@ -25,36 +19,3 @@ def list_attributes(o, only_public=True):
         def isattribute(o, attribute):
             return not callable(getattr(o, attribute))
     return [attribute for attribute in dir(o) if isattribute(o, attribute)]
-
-
-def make_directory(path):
-    try:
-        os.mkdir(path)
-    except OSError:
-        pass
-
-
-def remove(path):
-    try:
-        os.remove(path)
-    except OSError:
-        pass
-
-
-def touch(path):
-    with open(path, 'ab'):
-        pass
-
-
-def truncate(path):
-    with open(path, 'wb'):
-        pass
-
-
-def get_test_path(*args):
-    return os.path.join(temp_dir_path, *args)
-
-
-def make_temp_dir():
-    return tempfile.mkdtemp()
-
