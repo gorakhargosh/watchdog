ed17b08387fb2dd8361b2993573cfe8d97ff8f5b
Gora Khargosh <gora.khargosh@gmail.com>
2010-12-09 18:44:19 +0530

    Ignore temporary files in kqueue observer
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- watchdog/observers/kqueue.py
+++ watchdog/observers/kqueue.py
@@ -472,13 +472,16 @@ if platform.is_bsd() or platform.is_darwin():
                     # and then quickly deleted before we could open
                     # a descriptor for it. Therefore, simply queue a sequence
                     # of created and deleted events for the path.
-                    path = absolute_path(path)
-                    if is_directory:
-                        self.queue_event(DirCreatedEvent(path))
-                        self.queue_event(DirDeletedEvent(path))
-                    else:
-                        self.queue_event(FileCreatedEvent(path))
-                        self.queue_event(FileDeletedEvent(path))
+
+                    # TODO: We could simply ignore these files.
+                    pass
+                    #path = absolute_path(path)
+                    #if is_directory:
+                    #    self.queue_event(DirCreatedEvent(path))
+                    #    self.queue_event(DirDeletedEvent(path))
+                    #else:
+                    #    self.queue_event(FileCreatedEvent(path))
+                    #    self.queue_event(FileDeletedEvent(path))
                 else:
                     # All other errors are propagated.
                     raise
