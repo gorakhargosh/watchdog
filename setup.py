2c8e410ae7d8fb6473062d6026b5cbecd39d01ef
Filip Noetzel <filip@j03.de>
2010-12-09 15:14:32 +0100

    Use valid project/version requirement specifiers
    Fixes
    
    error: Setup script exited with error in watchdog setup command: 'install_requires' must be a string or list of strings containing valid project/version requirement specifiers

--- setup.py
+++ setup.py
@@ -78,18 +78,18 @@ ext_modules = {
     PLATFORM_WINDOWS: [],
 }
 
-common_install_requires = ['PyYAML (>=3.09)', 'argh (>=0.8.1)']
+common_install_requires = ['PyYAML >=3.09', 'argh >=0.8.1']
 if sys.version_info < (2, 7, 0) and \
         ('linux' in sys.platform or 'bsd' in sys.platform or 'darwin' in sys.platform):
     # Python 2.6 and below have the broken/non-existent kqueue implementations in the
     # select module. This backported patch adds it.
-    common_install_requires.append('select_backport (>=0.2)')
-    common_install_requires.append('argparse (>=1.1)')
+    common_install_requires.append('select_backport >=0.2')
+    common_install_requires.append('argparse >=1.1')
 
 install_requires = {
     PLATFORM_MACOSX: [],
-    PLATFORM_LINUX: ['pyinotify (>=0.9.1)'],
-    PLATFORM_WINDOWS: ['pywin32 (>=214)'],
+    PLATFORM_LINUX: ['pyinotify >=0.9.1'],
+    PLATFORM_WINDOWS: ['pywin32 >=214'],
 }
 
 scripts = [
