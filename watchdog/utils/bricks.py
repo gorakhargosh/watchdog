695b9e2b6ef454886c7ab432ccf1633813938902
Gora Khargosh <gora.khargosh@gmail.com>
2010-12-12 16:19:27 +0530

    Enables OrderedSet on supported Python versions
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- watchdog/utils/bricks.py
+++ watchdog/utils/bricks.py
@@ -43,6 +43,7 @@ Classes
 
 """
 
+import sys
 import collections
 try:
     import queue
@@ -110,81 +111,80 @@ class OrderedSetQueue(queue.Queue):
 
 
 
+if not sys.version < (2, 6, 0):
+    KEY, PREV, NEXT = range(3)
 
+    class OrderedSet(collections.MutableSet):
+        """
+        Implementation based on a doubly-linked link and an internal dictionary.
+        This design gives :class:`OrderedSet` the same big-Oh running times as
+        regular sets including O(1) adds, removes, and lookups as well as
+        O(n) iteration.
 
-KEY, PREV, NEXT = range(3)
+        .. ADMONITION:: Implementation notes
 
-class OrderedSet(collections.MutableSet):
-    """
-    Implementation based on a doubly-linked link and an internal dictionary.
-    This design gives :class:`OrderedSet` the same big-Oh running times as
-    regular sets including O(1) adds, removes, and lookups as well as
-    O(n) iteration.
+                Runs on Python 2.6 or later (and runs on Python 3.0 or later
+                without any modifications).
 
-    .. ADMONITION:: Implementation notes
+        :author: Raymond Hettinger <python@rcn.com>
+        :url: http://code.activestate.com/recipes/576694/
+        """
+        def __init__(self, iterable=None):
+            self.end = end = []
+            end += [None, end, end]         # sentinel node for doubly linked list
+            self.map = {}                   # key --> [key, prev, next]
+            if iterable is not None:
+                self |= iterable
 
-            Runs on Python 2.6 or later (and runs on Python 3.0 or later
-            without any modifications).
+        def __len__(self):
+            return len(self.map)
 
-    :author: Raymond Hettinger <python@rcn.com>
-    :url: http://code.activestate.com/recipes/576694/
-    """
-    def __init__(self, iterable=None):
-        self.end = end = []
-        end += [None, end, end]         # sentinel node for doubly linked list
-        self.map = {}                   # key --> [key, prev, next]
-        if iterable is not None:
-            self |= iterable
+        def __contains__(self, key):
+            return key in self.map
 
-    def __len__(self):
-        return len(self.map)
+        def add(self, key):
+            if key not in self.map:
+                end = self.end
+                curr = end[PREV]
+                curr[NEXT] = end[PREV] = self.map[key] = [key, curr, end]
 
-    def __contains__(self, key):
-        return key in self.map
+        def discard(self, key):
+            if key in self.map:
+                key, prev, next = self.map.pop(key)
+                prev[NEXT] = next
+                next[PREV] = prev
 
-    def add(self, key):
-        if key not in self.map:
+        def __iter__(self):
+            end = self.end
+            curr = end[NEXT]
+            while curr is not end:
+                yield curr[KEY]
+                curr = curr[NEXT]
+
+        def __reversed__(self):
             end = self.end
             curr = end[PREV]
-            curr[NEXT] = end[PREV] = self.map[key] = [key, curr, end]
+            while curr is not end:
+                yield curr[KEY]
+                curr = curr[PREV]
 
-    def discard(self, key):
-        if key in self.map:
-            key, prev, next = self.map.pop(key)
-            prev[NEXT] = next
-            next[PREV] = prev
+        def pop(self, last=True):
+            if not self:
+                raise KeyError('set is empty')
+            key = next(reversed(self)) if last else next(iter(self))
+            self.discard(key)
+            return key
 
-    def __iter__(self):
-        end = self.end
-        curr = end[NEXT]
-        while curr is not end:
-            yield curr[KEY]
-            curr = curr[NEXT]
+        def __repr__(self):
+            if not self:
+                return '%s()' % (self.__class__.__name__,)
+            return '%s(%r)' % (self.__class__.__name__, list(self))
 
-    def __reversed__(self):
-        end = self.end
-        curr = end[PREV]
-        while curr is not end:
-            yield curr[KEY]
-            curr = curr[PREV]
+        def __eq__(self, other):
+            if isinstance(other, OrderedSet):
+                return len(self) == len(other) and list(self) == list(other)
+            return set(self) == set(other)
 
-    def pop(self, last=True):
-        if not self:
-            raise KeyError('set is empty')
-        key = next(reversed(self)) if last else next(iter(self))
-        self.discard(key)
-        return key
-
-    def __repr__(self):
-        if not self:
-            return '%s()' % (self.__class__.__name__,)
-        return '%s(%r)' % (self.__class__.__name__, list(self))
-
-    def __eq__(self, other):
-        if isinstance(other, OrderedSet):
-            return len(self) == len(other) and list(self) == list(other)
-        return set(self) == set(other)
-
-    def __del__(self):
-        self.clear()                    # remove circular references
+        def __del__(self):
+            self.clear()                    # remove circular references
 
