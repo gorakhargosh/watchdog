2a759877072a1afa551e60bfebdc6b96ea0c179f
Martin Kreichgauer <kreichgauer@gmail.com>
2011-09-09 17:45:39 +0200

    terminate the `FSEventsEmitter.run` loop when calling `stop` (fix #64)

--- src/watchdog/observers/fsevents.py
+++ src/watchdog/observers/fsevents.py
@@ -71,7 +71,7 @@ if platform.is_darwin():
             self._lock = threading.Lock()
             self.snapshot = DirectorySnapshot(watch.path, watch.is_recursive)
 
-        def on_thread_exit(self):
+        def on_thread_told_to_stop(self):
             _fsevents.remove_watch(self.watch)
             _fsevents.stop(self)
 
