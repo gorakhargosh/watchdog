3e9f9a959b10a86296ca257b6991cd35ee3c2dbf
Shane Hathaway <shane@hathawaymix.org>
2012-01-12 00:20:39 -0700

    Added an AutoRestartTrick and an auto-restart watchmedo command.
    This is designed to ease development of long-running server processes.

--- src/watchdog/watchmedo.py
+++ src/watchdog/watchmedo.py
@@ -432,6 +432,96 @@ def shell_command(args):
     observe_with(observer, handler, args.directories, args.recursive)
 
 
+@arg('command',
+     help='''Long-running command to run in a subprocess.
+''')
+@arg('command_args',
+     metavar='arg',
+     nargs='*',
+     help='''Command arguments.
+
+Note: Use -- before the command arguments, otherwise watchmedo will
+try to interpret them.
+''')
+@arg('-d',
+     '--directory',
+     dest='directories',
+     nargs='*',
+     help='directory to watch')
+@arg('-p',
+     '--pattern',
+     '--patterns',
+     dest='patterns',
+     default='*',
+     help='matches event paths with these patterns (separated by ;).')
+@arg('-i',
+     '--ignore-pattern',
+     '--ignore-patterns',
+     dest='ignore_patterns',
+     default='',
+     help='ignores event paths with these patterns (separated by ;).')
+@arg('-D',
+     '--ignore-directories',
+     dest='ignore_directories',
+     default=False,
+     help='ignores events for directories')
+@arg('-R',
+     '--recursive',
+     dest='recursive',
+     default=False,
+     help='monitors the directories recursively')
+@arg('--interval',
+     '--timeout',
+     dest='timeout',
+     default=1.0,
+     help='use this as the polling interval/blocking timeout')
+@arg('--signal',
+     dest='signal',
+     default='SIGINT',
+     help='stop the subprocess with this signal (default SIGINT)')
+@arg('--kill-after',
+     dest='kill_after',
+     default=10.0,
+     help='when stopping, kill the subprocess after the specified timeout '
+        '(default 10)')
+def auto_restart(args):
+    """
+    Subcommand to start a long-running subprocess and restart it
+    on matched events.
+
+    :param args:
+        Command line argument options.
+    """
+    from watchdog.observers import Observer
+    from watchdog.tricks import AutoRestartTrick
+    import signal
+    import re
+
+    if not args.directories:
+        args.directories = ['.']
+
+    # Allow either signal name or number.
+    if re.match('^SIG[A-Z]+$', args.signal):
+        stop_signal = getattr(signal, args.signal)
+    else:
+        stop_signal = int(args.signal)
+
+    patterns, ignore_patterns = parse_patterns(args.patterns,
+                                               args.ignore_patterns)
+    command = [args.command]
+    command.extend(args.command_args)
+    handler = AutoRestartTrick(command=command,
+                                patterns=patterns,
+                                ignore_patterns=ignore_patterns,
+                                ignore_directories=args.ignore_directories,
+                                stop_signal=stop_signal,
+                                kill_after=args.kill_after)
+    handler.start()
+    observer = Observer(timeout=args.timeout)
+    observe_with(observer, handler, args.directories, args.recursive)
+    handler.stop()
+
+
 epilog = """Copyright (C) 2011 Yesudeep Mangalapilly <yesudeep@gmail.com>.
 
 Licensed under the terms of the Apache license, version 2.0. Please see
@@ -441,7 +531,8 @@ parser = ArghParser(epilog=epilog)
 parser.add_commands([tricks_from,
                      tricks_generate_yaml,
                      log,
-                     shell_command])
+                     shell_command,
+                     auto_restart])
 parser.add_argument('--version',
                     action='version',
                     version='%(prog)s ' + VERSION_STRING)
