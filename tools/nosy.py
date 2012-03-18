c6cf18c21b66c1c2e54259a4c763006a14aecf2b
Yesudeep Mangalapilly <yesudeep@google.com>
2012-03-18 12:32:35 +0530

    Fixes another copyright line.
    Signed-off-by: Yesudeep Mangalapilly <yesudeep@google.com>

--- tools/nosy.py
+++ tools/nosy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2010, 2011 Yesudeep Mangalapilly <yesudeep@gmail.com>
+# Copyright 2010, 2011 Yesudeep Mangalapilly <yesudeep@gmail.com>
 # Copyright 2012 Google, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
