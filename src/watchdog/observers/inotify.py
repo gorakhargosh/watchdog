b1e027117d9d159d5d3f7da7808e536a8029da8b
Thomas Amland <thomas.amland@gmail.com>
2013-05-12 17:45:00 +0200

    inotify: dir/tree might be gone when we try to add a watch for it

--- src/watchdog/observers/inotify.py
+++ src/watchdog/observers/inotify.py
@@ -582,18 +582,21 @@ if platform.is_linux():
             # IN_MOVED_TO events which don't pair up with
             # IN_MOVED_FROM events should be marked IN_CREATE
             # instead relative to this directory.
-            self._add_watch(src_path, self._event_mask)
+            try:
+              self._add_watch(src_path, self._event_mask)
+            except OSError:
+              continue
             for root, dirnames, filenames in os.walk(src_path):
               for dirname in dirnames:
-                full_path = absolute_path(
-                  os.path.join(root, dirname))
-                wd_dir = self._add_watch(full_path, self._event_mask)
-                event_list.append(InotifyEvent(wd_dir,
-                    InotifyConstants.IN_CREATE | InotifyConstants.IN_ISDIR,
-                    0, dirname, full_path))
+                try:
+                  full_path = absolute_path(os.path.join(root, dirname))
+                  wd_dir = self._add_watch(full_path, self._event_mask)
+                  event_list.append(InotifyEvent(wd_dir,
+                      InotifyConstants.IN_CREATE | InotifyConstants.IN_ISDIR, 0, dirname, full_path))
+                except OSError:
+                  pass
               for filename in filenames:
-                full_path = absolute_path(
-                  os.path.join(root, filename))
+                full_path = absolute_path(os.path.join(root, filename))
                 wd_parent_dir = self._wd_for_path[absolute_path(os.path.dirname(full_path))]
                 event_list.append(InotifyEvent(wd_parent_dir,
                     InotifyConstants.IN_CREATE, 0, filename, full_path))
