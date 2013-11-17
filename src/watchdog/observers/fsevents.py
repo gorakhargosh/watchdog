a1e0d8c4290eab71f3fc9b1a301dc7081034e0a4
Peter M. Elias <petermelias@gmail.com>
2013-11-17 09:08:34 -0500

    cosmetic / semantic refactor of the observer import system, slight redesign. added backcompat importlib2 to watchdog.utils

--- src/watchdog/observers/fsevents.py
+++ src/watchdog/observers/fsevents.py
@@ -26,136 +26,140 @@
 from __future__ import with_statement
 from watchdog.utils import platform
 
-if platform.is_darwin():
-    import threading
-    import unicodedata
-    import _watchdog_fsevents as _fsevents
+if not platform.is_darwin():
+    raise ImportError
 
-    from watchdog.events import (
-        FileDeletedEvent,
-        FileModifiedEvent,
-        FileCreatedEvent,
-        FileMovedEvent,
-        DirDeletedEvent,
-        DirModifiedEvent,
-        DirCreatedEvent,
-        DirMovedEvent
-    )
+import threading
+import unicodedata
+import _watchdog_fsevents as _fsevents
 
-    from watchdog.utils.dirsnapshot import DirectorySnapshot
-    from watchdog.observers.api import (
-        BaseObserver,
-        EventEmitter,
-        DEFAULT_EMITTER_TIMEOUT,
-        DEFAULT_OBSERVER_TIMEOUT
-    )
+from watchdog.events import (
+    FileDeletedEvent,
+    FileModifiedEvent,
+    FileCreatedEvent,
+    FileMovedEvent,
+    DirDeletedEvent,
+    DirModifiedEvent,
+    DirCreatedEvent,
+    DirMovedEvent
+)
 
-    class FSEventsEmitter(EventEmitter):
+from watchdog.utils.dirsnapshot import DirectorySnapshot
+from watchdog.observers.api import (
+    BaseObserver,
+    EventEmitter,
+    DEFAULT_EMITTER_TIMEOUT,
+    DEFAULT_OBSERVER_TIMEOUT
+)
 
-        """
-        Mac OS X FSEvents Emitter class.
 
-        :param event_queue:
-            The event queue to fill with events.
-        :param watch:
-            A watch object representing the directory to monitor.
-        :type watch:
-            :class:`watchdog.observers.api.ObservedWatch`
-        :param timeout:
-            Read events blocking timeout (in seconds).
-        :type timeout:
-            ``float``
-        """
+class FSEventsEmitter(EventEmitter):
 
-        def __init__(self, event_queue, watch, timeout=DEFAULT_EMITTER_TIMEOUT):
-            EventEmitter.__init__(self, event_queue, watch, timeout)
-            self._lock = threading.Lock()
-            self.snapshot = DirectorySnapshot(watch.path, watch.is_recursive)
+    """
+    Mac OS X FSEvents Emitter class.
 
-        def on_thread_stop(self):
-            _fsevents.remove_watch(self.watch)
-            _fsevents.stop(self)
+    :param event_queue:
+        The event queue to fill with events.
+    :param watch:
+        A watch object representing the directory to monitor.
+    :type watch:
+        :class:`watchdog.observers.api.ObservedWatch`
+    :param timeout:
+        Read events blocking timeout (in seconds).
+    :type timeout:
+        ``float``
+    """
 
-        def queue_events(self, timeout):
-            with self._lock:
-                if not self.watch.is_recursive\
-                    and self.watch.path not in self.pathnames:
-                    return
-                new_snapshot = DirectorySnapshot(self.watch.path,
-                                                 self.watch.is_recursive)
-                events = new_snapshot - self.snapshot
-                self.snapshot = new_snapshot
+    def __init__(self, event_queue, watch, timeout=DEFAULT_EMITTER_TIMEOUT):
+        EventEmitter.__init__(self, event_queue, watch, timeout)
+        self._lock = threading.Lock()
+        self.snapshot = DirectorySnapshot(watch.path, watch.is_recursive)
 
-                # Files.
-                for src_path in events.files_deleted:
-                    self.queue_event(FileDeletedEvent(src_path))
-                for src_path in events.files_modified:
-                    self.queue_event(FileModifiedEvent(src_path))
-                for src_path in events.files_created:
-                    self.queue_event(FileCreatedEvent(src_path))
-                for src_path, dest_path in events.files_moved:
-                    self.queue_event(FileMovedEvent(src_path, dest_path))
+    def on_thread_stop(self):
+        _fsevents.remove_watch(self.watch)
+        _fsevents.stop(self)
 
-                # Directories.
-                for src_path in events.dirs_deleted:
-                    self.queue_event(DirDeletedEvent(src_path))
-                for src_path in events.dirs_modified:
-                    self.queue_event(DirModifiedEvent(src_path))
-                for src_path in events.dirs_created:
-                    self.queue_event(DirCreatedEvent(src_path))
-                for src_path, dest_path in events.dirs_moved:
-                    self.queue_event(DirMovedEvent(src_path, dest_path))
+    def queue_events(self, timeout):
+        with self._lock:
+            if not self.watch.is_recursive\
+                and self.watch.path not in self.pathnames:
+                return
+            new_snapshot = DirectorySnapshot(self.watch.path,
+                                             self.watch.is_recursive)
+            events = new_snapshot - self.snapshot
+            self.snapshot = new_snapshot
 
-        def run(self):
-            try:
-                def callback(pathnames, flags, emitter=self):
-                    emitter.queue_events(emitter.timeout)
+            # Files.
+            for src_path in events.files_deleted:
+                self.queue_event(FileDeletedEvent(src_path))
+            for src_path in events.files_modified:
+                self.queue_event(FileModifiedEvent(src_path))
+            for src_path in events.files_created:
+                self.queue_event(FileCreatedEvent(src_path))
+            for src_path, dest_path in events.files_moved:
+                self.queue_event(FileMovedEvent(src_path, dest_path))
 
-                # for pathname, flag in zip(pathnames, flags):
-                # if emitter.watch.is_recursive: # and pathname != emitter.watch.path:
-                #    new_sub_snapshot = DirectorySnapshot(pathname, True)
-                #    old_sub_snapshot = self.snapshot.copy(pathname)
-                #    diff = new_sub_snapshot - old_sub_snapshot
-                #    self.snapshot += new_subsnapshot
-                # else:
-                #    new_snapshot = DirectorySnapshot(emitter.watch.path, False)
-                #    diff = new_snapshot - emitter.snapshot
-                #    emitter.snapshot = new_snapshot
+            # Directories.
+            for src_path in events.dirs_deleted:
+                self.queue_event(DirDeletedEvent(src_path))
+            for src_path in events.dirs_modified:
+                self.queue_event(DirModifiedEvent(src_path))
+            for src_path in events.dirs_created:
+                self.queue_event(DirCreatedEvent(src_path))
+            for src_path, dest_path in events.dirs_moved:
+                self.queue_event(DirMovedEvent(src_path, dest_path))
 
-                # INFO: FSEvents reports directory notifications recursively
-                # by default, so we do not need to add subdirectory paths.
-                #pathnames = set([self.watch.path])
-                # if self.watch.is_recursive:
-                #    for root, directory_names, _ in os.walk(self.watch.path):
-                #        for directory_name in directory_names:
-                #            full_path = absolute_path(
-                #                            os.path.join(root, directory_name))
-                #            pathnames.add(full_path)
-                self.pathnames = [self.watch.path]
-                _fsevents.add_watch(self,
-                                    self.watch,
-                                    callback,
-                                    self.pathnames)
-                _fsevents.read_events(self)
-            except Exception as e:
-                pass
+    def run(self):
+        try:
+            def callback(pathnames, flags, emitter=self):
+                emitter.queue_events(emitter.timeout)
 
-    class FSEventsObserver(BaseObserver):
+            # for pathname, flag in zip(pathnames, flags):
+            # if emitter.watch.is_recursive: # and pathname != emitter.watch.path:
+            #    new_sub_snapshot = DirectorySnapshot(pathname, True)
+            #    old_sub_snapshot = self.snapshot.copy(pathname)
+            #    diff = new_sub_snapshot - old_sub_snapshot
+            #    self.snapshot += new_subsnapshot
+            # else:
+            #    new_snapshot = DirectorySnapshot(emitter.watch.path, False)
+            #    diff = new_snapshot - emitter.snapshot
+            #    emitter.snapshot = new_snapshot
 
-        def __init__(self, timeout=DEFAULT_OBSERVER_TIMEOUT):
-            BaseObserver.__init__(self, emitter_class=FSEventsEmitter,
-                                  timeout=timeout)
+            # INFO: FSEvents reports directory notifications recursively
+            # by default, so we do not need to add subdirectory paths.
+            #pathnames = set([self.watch.path])
+            # if self.watch.is_recursive:
+            #    for root, directory_names, _ in os.walk(self.watch.path):
+            #        for directory_name in directory_names:
+            #            full_path = absolute_path(
+            #                            os.path.join(root, directory_name))
+            #            pathnames.add(full_path)
+            self.pathnames = [self.watch.path]
+            _fsevents.add_watch(self,
+                                self.watch,
+                                callback,
+                                self.pathnames)
+            _fsevents.read_events(self)
+        except Exception as e:
+            pass
 
-        def schedule(self, event_handler, path, recursive=False):
-            # Python 2/3 compat
-            try:
-                str_class = unicode
-            except NameError:
-                str_class = str
 
-            # Fix for issue #26: Trace/BPT error when given a unicode path
-            # string. https://github.com/gorakhargosh/watchdog/issues#issue/26
-            if isinstance(path, str_class):
-                #path = unicode(path, 'utf-8')
-                path = unicodedata.normalize('NFC', path).encode('utf-8')
-            return BaseObserver.schedule(self, event_handler, path, recursive)
+class FSEventsObserver(BaseObserver):
+
+    def __init__(self, timeout=DEFAULT_OBSERVER_TIMEOUT):
+        BaseObserver.__init__(self, emitter_class=FSEventsEmitter,
+                              timeout=timeout)
+
+    def schedule(self, event_handler, path, recursive=False):
+        # Python 2/3 compat
+        try:
+            str_class = unicode
+        except NameError:
+            str_class = str
+
+        # Fix for issue #26: Trace/BPT error when given a unicode path
+        # string. https://github.com/gorakhargosh/watchdog/issues#issue/26
+        if isinstance(path, str_class):
+            #path = unicode(path, 'utf-8')
+            path = unicodedata.normalize('NFC', path).encode('utf-8')
+        return BaseObserver.schedule(self, event_handler, path, recursive)
--- src/watchdog/observers/read_directory_changes.py
+++ src/watchdog/observers/read_directory_changes.py
@@ -20,128 +20,132 @@
 from __future__ import with_statement
 from watchdog.utils import platform
 
-if platform.is_windows():
-    import ctypes
-    import threading
-    import os.path
-    import time
+if not platform.is_windows():
+    raise ImportError
 
-    from pathtools.path import absolute_path
+import ctypes
+import threading
+import os.path
+import time
 
-    from watchdog.observers.winapi_common import (
-        DIR_ACTION_EVENT_MAP,
-        FILE_ACTION_EVENT_MAP,
-        WATCHDOG_FILE_FLAGS,
-        WATCHDOG_TRAVERSE_MOVED_DIR_DELAY,
-        read_directory_changes,
-        get_directory_handle,
-        close_directory_handle,
-        BUFFER_SIZE
-    )
+from pathtools.path import absolute_path
 
-    from watchdog.observers.winapi import (
-        FILE_ACTION_RENAMED_OLD_NAME,
-        FILE_ACTION_RENAMED_NEW_NAME,
-        get_FILE_NOTIFY_INFORMATION
-    )
+from watchdog.observers.winapi_common import (
+    DIR_ACTION_EVENT_MAP,
+    FILE_ACTION_EVENT_MAP,
+    WATCHDOG_FILE_FLAGS,
+    WATCHDOG_TRAVERSE_MOVED_DIR_DELAY,
+    read_directory_changes,
+    get_directory_handle,
+    close_directory_handle,
+    BUFFER_SIZE
+)
 
-    from watchdog.observers.api import (
-        EventEmitter,
-        BaseObserver,
-        DEFAULT_OBSERVER_TIMEOUT,
-        DEFAULT_EMITTER_TIMEOUT
-    )
+from watchdog.observers.winapi import (
+    FILE_ACTION_RENAMED_OLD_NAME,
+    FILE_ACTION_RENAMED_NEW_NAME,
+    get_FILE_NOTIFY_INFORMATION
+)
 
-    from watchdog.events import (
-        DirCreatedEvent,
-        DirMovedEvent,
-        FileCreatedEvent,
-        FileMovedEvent
-    )
+from watchdog.observers.api import (
+    EventEmitter,
+    BaseObserver,
+    DEFAULT_OBSERVER_TIMEOUT,
+    DEFAULT_EMITTER_TIMEOUT
+)
 
-    class WindowsApiEmitter(EventEmitter):
+from watchdog.events import (
+    DirCreatedEvent,
+    DirMovedEvent,
+    FileCreatedEvent,
+    FileMovedEvent
+)
 
-        """
-        Windows API-based emitter that uses ReadDirectoryChangesW
-        to detect file system changes for a watch.
-        """
 
-        def __init__(self, event_queue, watch, timeout=DEFAULT_EMITTER_TIMEOUT):
-            EventEmitter.__init__(self, event_queue, watch, timeout)
-            self._lock = threading.Lock()
+class WindowsApiEmitter(EventEmitter):
 
-            self._directory_handle = get_directory_handle(watch.path,
-                                                          WATCHDOG_FILE_FLAGS)
-            self._buffer = ctypes.create_string_buffer(BUFFER_SIZE)
+    """
+    Windows API-based emitter that uses ReadDirectoryChangesW
+    to detect file system changes for a watch.
+    """
 
-        def on_thread_stop(self):
-            close_directory_handle(self._directory_handle)
+    def __init__(self, event_queue, watch, timeout=DEFAULT_EMITTER_TIMEOUT):
+        EventEmitter.__init__(self, event_queue, watch, timeout)
+        self._lock = threading.Lock()
 
-        def queue_events(self, timeout):
-            with self._lock:
-                dir_changes, nbytes = read_directory_changes(
-                    self._directory_handle,
-                    self._buffer,
-                    self.watch.is_recursive
-                )
+        self._directory_handle = get_directory_handle(watch.path,
+                                                      WATCHDOG_FILE_FLAGS)
+        self._buffer = ctypes.create_string_buffer(BUFFER_SIZE)
 
-                last_renamed_src_path = ""
-                for action, src_path in get_FILE_NOTIFY_INFORMATION(dir_changes, nbytes):
-                    src_path = absolute_path(os.path.join(self.watch.path,
-                                                          src_path))
+    def on_thread_stop(self):
+        close_directory_handle(self._directory_handle)
 
-                    if action == FILE_ACTION_RENAMED_OLD_NAME:
-                        last_renamed_src_path = src_path
-                    elif action == FILE_ACTION_RENAMED_NEW_NAME:
-                        dest_path = src_path
-                        src_path = last_renamed_src_path
+    def queue_events(self, timeout):
+        with self._lock:
+            dir_changes, nbytes = read_directory_changes(
+                self._directory_handle,
+                self._buffer,
+                self.watch.is_recursive
+            )
 
-                        if os.path.isdir(dest_path):
-                            event = DirMovedEvent(src_path, dest_path)
-                            if self.watch.is_recursive:
-                                # HACK: We introduce a forced delay before
-                                # traversing the moved directory. This will read
-                                # only file movement that finishes within this
-                                # delay time.
-                                time.sleep(WATCHDOG_TRAVERSE_MOVED_DIR_DELAY)
-                                # The following block of code may not
-                                # obtain moved events for the entire tree if
-                                # the I/O is not completed within the above
-                                # delay time. So, it's not guaranteed to work.
-                                # TODO: Come up with a better solution, possibly
-                                # a way to wait for I/O to complete before
-                                # queuing events.
-                                for sub_moved_event in event.sub_moved_events():
-                                    self.queue_event(sub_moved_event)
-                            self.queue_event(event)
-                        else:
-                            self.queue_event(FileMovedEvent(src_path, dest_path))
+            last_renamed_src_path = ""
+            for action, src_path in get_FILE_NOTIFY_INFORMATION(dir_changes, nbytes):
+                src_path = absolute_path(os.path.join(self.watch.path,
+                                                      src_path))
+
+                if action == FILE_ACTION_RENAMED_OLD_NAME:
+                    last_renamed_src_path = src_path
+                elif action == FILE_ACTION_RENAMED_NEW_NAME:
+                    dest_path = src_path
+                    src_path = last_renamed_src_path
+
+                    if os.path.isdir(dest_path):
+                        event = DirMovedEvent(src_path, dest_path)
+                        if self.watch.is_recursive:
+                            # HACK: We introduce a forced delay before
+                            # traversing the moved directory. This will read
+                            # only file movement that finishes within this
+                            # delay time.
+                            time.sleep(WATCHDOG_TRAVERSE_MOVED_DIR_DELAY)
+                            # The following block of code may not
+                            # obtain moved events for the entire tree if
+                            # the I/O is not completed within the above
+                            # delay time. So, it's not guaranteed to work.
+                            # TODO: Come up with a better solution, possibly
+                            # a way to wait for I/O to complete before
+                            # queuing events.
+                            for sub_moved_event in event.sub_moved_events():
+                                self.queue_event(sub_moved_event)
+                        self.queue_event(event)
                     else:
-                        if os.path.isdir(src_path):
-                            event = DIR_ACTION_EVENT_MAP[action](src_path)
-                            if isinstance(event, DirCreatedEvent):
-                                # If a directory is moved from outside the watched folder to inside it
-                                # we only get a created directory event out of it, not any events for its children
-                                # so use the same hack as for file moves to get the child events
-                                time.sleep(WATCHDOG_TRAVERSE_MOVED_DIR_DELAY)
-                                sub_events = _generate_sub_created_events_for(src_path)
-                                for sub_created_event in sub_events:
-                                    self.queue_event(sub_created_event)
-                            self.queue_event(event)
-                        else:
-                            self.queue_event(FILE_ACTION_EVENT_MAP[action](src_path))
+                        self.queue_event(FileMovedEvent(src_path, dest_path))
+                else:
+                    if os.path.isdir(src_path):
+                        event = DIR_ACTION_EVENT_MAP[action](src_path)
+                        if isinstance(event, DirCreatedEvent):
+                            # If a directory is moved from outside the watched folder to inside it
+                            # we only get a created directory event out of it, not any events for its children
+                            # so use the same hack as for file moves to get the child events
+                            time.sleep(WATCHDOG_TRAVERSE_MOVED_DIR_DELAY)
+                            sub_events = _generate_sub_created_events_for(src_path)
+                            for sub_created_event in sub_events:
+                                self.queue_event(sub_created_event)
+                        self.queue_event(event)
+                    else:
+                        self.queue_event(FILE_ACTION_EVENT_MAP[action](src_path))
 
-    class WindowsApiObserver(BaseObserver):
 
-        """
-        Observer thread that schedules watching directories and dispatches
-        calls to event handlers.
-        """
+class WindowsApiObserver(BaseObserver):
 
-        def __init__(self, timeout=DEFAULT_OBSERVER_TIMEOUT):
-            BaseObserver.__init__(self,
-                                  emitter_class=WindowsApiEmitter,
-                                  timeout=timeout)
+    """
+    Observer thread that schedules watching directories and dispatches
+    calls to event handlers.
+    """
+
+    def __init__(self, timeout=DEFAULT_OBSERVER_TIMEOUT):
+        BaseObserver.__init__(self,
+                              emitter_class=WindowsApiEmitter,
+                              timeout=timeout)
 
 
 def _generate_sub_created_events_for(src_dir_path):
