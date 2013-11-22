ee30e78294932addb356a6785c6a9505a68ee2f9
Thomas Amland <thomas.amland@gmail.com>
2013-11-22 16:22:04 +0100

    remove unused constant ALL_INOTIFY_BITS

--- src/watchdog/observers/inotify_c.py
+++ src/watchdog/observers/inotify_c.py
@@ -118,31 +118,6 @@ class InotifyConstants(object):
     IN_CLOEXEC = 0x02000000
     IN_NONBLOCK = 0x00004000
 
-    # All inotify bits.
-    ALL_INOTIFY_BITS = reduce(
-        lambda x, y: x | y, [
-            IN_ACCESS,
-            IN_MODIFY,
-            IN_ATTRIB,
-            IN_CLOSE_WRITE,
-            IN_CLOSE_NOWRITE,
-            IN_OPEN,
-            IN_MOVED_FROM,
-            IN_MOVED_TO,
-            IN_CREATE,
-            IN_DELETE,
-            IN_DELETE_SELF,
-            IN_MOVE_SELF,
-            IN_UNMOUNT,
-            IN_Q_OVERFLOW,
-            IN_IGNORED,
-            IN_ONLYDIR,
-            IN_DONT_FOLLOW,
-            IN_EXCL_UNLINK,
-            IN_MASK_ADD,
-            IN_ISDIR,
-            IN_ONESHOT,
-        ])
 
 # Watchdog's API cares only about these events.
 WATCHDOG_ALL_EVENTS = reduce(
