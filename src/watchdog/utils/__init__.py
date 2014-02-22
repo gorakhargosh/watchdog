5200577607a099a5ffd28c2b0d3bebcc1ac2f7bc
Thomas Amland <thomas.amland@gmail.com>
2014-02-22 08:27:48 -0800

    use default stat result struct. added memory consumption seems insignificant

--- src/watchdog/utils/__init__.py
+++ src/watchdog/utils/__init__.py
@@ -48,17 +48,12 @@ import watchdog.utils.platform
 from collections import namedtuple
 
 
-StatResult = namedtuple('StatResult', 'st_ino st_mode st_mtime')
-
 if sys.version_info[0] == 2 and platform.is_windows():
     # st_ino is not implemented in os.stat on this platform
     import win32stat
     stat = win32stat.stat
 else:
-    def _stat(path):
-        st = os.stat(path)
-        return StatResult(st.st_ino, st.st_mode, st.st_mtime)
-    stat = _stat
+    stat = os.stat
 
 
 def ctypes_find_library(name, default):
