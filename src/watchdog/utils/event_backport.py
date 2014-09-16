f34d47578a712d5eec8f93fad023a909f78eb189
Thomas Amland <thomas.amland@gmail.com>
2014-09-16 20:13:57 +0200

    whoops. backported wrong version

--- src/watchdog/utils/event_backport.py
+++ src/watchdog/utils/event_backport.py
@@ -4,78 +4,38 @@
 
 from threading import Condition, Lock
 
-class Event:
-    """Class implementing event objects.
 
-    Events manage a flag that can be set to true with the set() method and reset
-    to false with the clear() method. The wait() method blocks until the flag is
-    true.  The flag is initially false.
+class Event(object):
 
-    """
+    def __init__(self,):
+        self.__cond = Condition(Lock())
+        self.__flag = False
 
-    # After Tim Peters' event class (without is_posted())
+    def isSet(self):
+        return self.__flag
 
-    def __init__(self):
-        self._cond = Condition(Lock())
-        self._flag = False
-
-    def _reset_internal_locks(self):
-        # private!  called by Thread._reset_internal_locks by _after_fork()
-        self._cond.__init__()
-
-    def is_set(self):
-        """Return true if and only if the internal flag is true."""
-        return self._flag
-
-    isSet = is_set
+    is_set = isSet
 
     def set(self):
-        """Set the internal flag to true.
-
-        All threads waiting for it to become true are awakened. Threads
-        that call wait() once the flag is true will not block at all.
-
-        """
-        self._cond.acquire()
+        self.__cond.acquire()
         try:
-            self._flag = True
-            self._cond.notify_all()
+            self.__flag = True
+            self.__cond.notify_all()
         finally:
-            self._cond.release()
+            self.__cond.release()
 
     def clear(self):
-        """Reset the internal flag to false.
-
-        Subsequently, threads calling wait() will block until set() is called to
-        set the internal flag to true again.
-
-        """
-        self._cond.acquire()
+        self.__cond.acquire()
         try:
-            self._flag = False
+            self.__flag = False
         finally:
-            self._cond.release()
+            self.__cond.release()
 
     def wait(self, timeout=None):
-        """Block until the internal flag is true.
-
-        If the internal flag is true on entry, return immediately. Otherwise,
-        block until another thread calls set() to set the flag to true, or until
-        the optional timeout occurs.
-
-        When the timeout argument is present and not None, it should be a
-        floating point number specifying a timeout for the operation in seconds
-        (or fractions thereof).
-
-        This method returns the internal flag on exit, so it will always return
-        True except if a timeout is given and the operation times out.
-
-        """
-        self._cond.acquire()
+        self.__cond.acquire()
         try:
-            signaled = self._flag
-            if not signaled:
-                signaled = self._cond.wait(timeout)
-            return signaled
+            if not self.__flag:
+                self.__cond.wait(timeout)
+            return self.__flag
         finally:
-            self._cond.release()
+            self.__cond.release()
