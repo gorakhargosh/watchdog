gorakhargosh/watchdog

commit 2e831999218814170a118b234a792917ccaa9ff7

Avoid race condition crash on directory deletion
Given the directory structure /a/b/c.txt there is a race condition
in DirectorySnapshot if it reads the list of entries in /a, then
/a/b is deleted, and then it tries to read /a/b. This happens
often in practice when changing between very different branches in
git.

The ideal behaviour would be to report /a/b as not existing in this
case, but we cannot do this without either changing the order of the
walk (from parent-first to parent-last) or significantly increasing
memory usage and copies, so instead we report /a/b as existing but
empty.

src/watchdog/utils/dirsnapshot.py
@@ -44,6 +44,7 @@ Classes
 
 """
 
+import errno
 import os
 from stat import S_ISDIR
 from watchdog.utils import platform
@@ -208,7 +209,16 @@ class DirectorySnapshot(object):
         self._inode_to_path[(st.st_ino, st.st_dev)] = path
 
         def walk(root):
-            paths = [os.path.join(root, name) for name in listdir(root)]
+            try:
+                paths = [os.path.join(root, name) for name in listdir(root)]
+            except OSError as e:
+                # Directory may have been deleted between finding it in the directory
+                # list of its parent and trying to delete its contents. If this
+                # happens we treat it as empty.
+                if e.errno == errno.ENOENT:
+                    return
+                else:
+                    raise
             entries = []
             for p in paths:
                 try:

commit 0e4adc4c6ea4ceb18e9d86c354b0a399dedff81e

Avoid race condition crash when a directory is swapped for a file
This commit builds off the race condition checking introduced in
2e8319992, but also correctly handles the case where a directory is
replaced with a file before it can be walked.

For example:
1. [external] create /a/b/c.txt
2. [watchdog] walk /a (contains directory /a/b/ at this point)
3. [external] rm -rf /a/b/
4. [external] touch /a/b
5. [watchdog] walk /a/b/ (raises NotADirectoryError)

src/watchdog/utils/dirsnapshot.py
@@ -44,7 +44,6 @@ Classes
 
 """
 
-import errno
 import os
 from stat import S_ISDIR
 from watchdog.utils import platform
@@ -211,14 +210,12 @@ class DirectorySnapshot(object):
         def walk(root):
             try:
                 paths = [os.path.join(root, name) for name in listdir(root)]
-            except OSError as e:
+            except (FileNotFoundError, NotADirectoryError):
                 # Directory may have been deleted between finding it in the directory
                 # list of its parent and trying to delete its contents. If this
-                # happens we treat it as empty.
-                if e.errno == errno.ENOENT:
-                    return
-                else:
-                    raise
+                # happens we treat it as empty. Likewise if the directory was replaced
+                # with a file of the same name (less likely, but possible).
+                return
             entries = []
             for p in paths:
                 try: