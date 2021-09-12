cef5b908ff3f5bbe62d2ad072ed35de3e1ebb889
AndreaRe9 <59319678+AndreaRe9@users.noreply.github.com>
2021-09-12 14:39:46 +0200

    Removed references to argh

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ if is_macos or os.getenv('FORCE_MACOS_MACHINE', '0') == '1':
     ]
 
 extras_require = {
-    'watchmedo': ['PyYAML>=3.10', 'argh>=0.24.1'],
+    'watchmedo': ['PyYAML>=3.10'],
 }
 
 with open('README.rst', encoding='utf-8') as f:
