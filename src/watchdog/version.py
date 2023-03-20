da09c060a007fe7fddde27592e4f63ae1e8697bc
Mickaël Schoentgen <contact@tiger-222.fr>
2023-03-20 10:06:33 +0100

    Release 3.0.0

--- src/watchdog/version.py
+++ src/watchdog/version.py
@@ -18,9 +18,9 @@ from __future__ import annotations
 
 # When updating this version number, please update the
 # ``docs/source/global.rst.inc`` file as well.
-VERSION_MAJOR = 2
-VERSION_MINOR = 3
-VERSION_BUILD = 2
+VERSION_MAJOR = 3
+VERSION_MINOR = 0
+VERSION_BUILD = 0
 VERSION_INFO = (VERSION_MAJOR, VERSION_MINOR, VERSION_BUILD)
 VERSION_STRING = f"{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_BUILD}"
 
