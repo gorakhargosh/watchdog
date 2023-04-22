af9436edf601f140dd693f73e78efde9294482e5
Mickaël Schoentgen <contact@tiger-222.fr>
2023-04-22 17:13:01 +0200

    Update test_fsevents.py

--- tests/test_fsevents.py
+++ tests/test_fsevents.py
@@ -262,6 +262,7 @@ def test_recursive_check_accepts_relative_paths(p: P) -> None:
             self.observed_events = set()
 
         def on_any_event(self, event):
+            print(event)
             self.expected_events.remove(event)
             self.observed_events.add(event)
 
