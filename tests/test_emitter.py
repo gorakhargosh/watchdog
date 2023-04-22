4622197cabee171291fe73429237615121aab110
Mickaël Schoentgen <contact@tiger-222.fr>
2023-04-22 17:02:11 +0200

    Update test_emitter.py

--- tests/test_emitter.py
+++ tests/test_emitter.py
@@ -354,9 +354,11 @@ def test_recursive_on(p: P, start_watching: StartWatching, expect_event: ExpectE
 
     if not platform.is_windows():
         expect_event(DirModifiedEvent(p("dir1", "dir2", "dir3")))
-        expect_event(FileOpenedEvent(p("dir1", "dir2", "dir3", "a")))
+        if platform.is_linux():
+            expect_event(FileOpenedEvent(p("dir1", "dir2", "dir3", "a")))
         expect_event(FileAttribEvent(p("dir1", "dir2", "dir3", "a")))
-        expect_event(FileClosedEvent(p("dir1", "dir2", "dir3", "a")))
+        if platform.is_linux():
+            expect_event(FileClosedEvent(p("dir1", "dir2", "dir3", "a")))
 
 
 @pytest.mark.flaky(max_runs=5, min_passes=1, rerun_filter=rerun_filter)
@@ -378,9 +380,9 @@ def test_recursive_off(
     if not platform.is_windows():
         expect_event(DirModifiedEvent(p()))
 
-        if platform.is_linux():
-            expect_event(FileOpenedEvent(p("b")))
-            expect_event(FileClosedEvent(p("b")))
+    if platform.is_linux():
+        expect_event(FileOpenedEvent(p("b")))
+        expect_event(FileClosedEvent(p("b")))
 
     # currently limiting these additional events to macOS only, see https://github.com/gorakhargosh/watchdog/pull/779
     if platform.is_darwin():
