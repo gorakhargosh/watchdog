698edea4f59eaeb1eb2d96614411ce61dfcc9e8c
Nicholas Hairs <info@nicholashairs.com>
2024-10-26 17:24:56 +1100

    Update development status classifier
    This project is pretty stable now

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ setup(
         ]
     ),
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "Intended Audience :: System Administrators",
